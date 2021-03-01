71b45c5b0ce34557dac37570dca8f1e9b8bde6ed
nimrp01 <40931275+nimrp01@users.noreply.github.com>
2021-03-01 14:54:20 +0545

    Update compare_test.py

--- compare_test.py
+++ compare_test.py
@@ -4,7 +4,7 @@ import os
 lngth = len(os.listdir('figures/qual_results/sketchy/13'))
 print(lngth)
 for i in range(lngth):
-  #cmp_ssim --first (os.listdir('D:\Masters\\figures\qual_results\sketchy\\3')[i]) --second (os.listdir('D:\Masters\\figures\qual_results\sketchy\\3')[i+1])
+  cmp_ssim --first (os.listdir('figures/qual_results/sketchy/13')[i]) --second (os.listdir('figures/qual_results/sketchy/13')[i+1])
   cmp_ssim --first os.listdir('figures/qual_results/sketchy/23')[i] --second os.listdir('figures/qual_results/sketchy/23')[i+1]
   cmp_ssim --first os.listdir('figures/qual_results/sketchy/27')[i] --second os.listdir('figures/qual_results/sketchy/27')[i+1]
   cmp_ssim --first os.listdir('figures/qual_results/sketchy/3')[i] --second os.listdir('figures/qual_results/sketchy/3')[i+1]
