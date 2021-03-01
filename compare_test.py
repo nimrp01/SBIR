c5e8967e3e73b8b637f247fd2c1525bbbf019451
nimrp01 <40931275+nimrp01@users.noreply.github.com>
2021-03-01 14:58:59 +0545

    Update compare_test.py

--- compare_test.py
+++ compare_test.py
@@ -1,11 +1,8 @@
 from compare_ssim import cmp_ssim
 import os
 
-lngth = len(os.listdir('figures/qual_results/sketchy/13'))
+lngth13 = os.listdir('figures/qual_results/sketchy/13')
 print(lngth)
-for i in range(lngth):
-  cmp_ssim --first (os.listdir('figures/qual_results/sketchy/13')[i]) --second (os.listdir('figures/qual_results/sketchy/13')[i+1])
-  cmp_ssim --first os.listdir('figures/qual_results/sketchy/23')[i] --second os.listdir('figures/qual_results/sketchy/23')[i+1]
-  cmp_ssim --first os.listdir('figures/qual_results/sketchy/27')[i] --second os.listdir('figures/qual_results/sketchy/27')[i+1]
-  cmp_ssim --first os.listdir('figures/qual_results/sketchy/3')[i] --second os.listdir('figures/qual_results/sketchy/3')[i+1]
-  cmp_ssim --first os.listdir('figures/qual_results/sketchy/4')[i] --second os.listdir('figures/qual_results/sketchy/4')[i+1]
+for i in range(len(lngth13)):
+  cmp_ssim --first lngth13[i] --second lngth13[i+1]
+
