6a32d2055b2036c5ccb3bd2d09a10098aca3e375
nimrp01 <40931275+nimrp01@users.noreply.github.com>
2021-03-03 13:13:15 +0545

    Update compare_mse.py

--- compare_mse.py
+++ compare_mse.py
@@ -18,7 +18,7 @@ def mse(imageA, imageB):
     # the 'Mean Squared Error' between the two images is the
     # sum of the squared difference between the two images;
     # NOTE: the two images must have the same dimension
-    print(imageA)
+#     print(imageA)
     err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
     err /= float(imageA.shape[0] * imageA.shape[1])
 
